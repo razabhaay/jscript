6a053e40cfd9ca4131bbcca91fd53d5a341a56e2
razabhaay <razabhaay@gmail.com>
2026-04-25 11:30:31 +0000

    conversion

---
+++ basic1/conversionOperation.js
@@ -0,0 +1,43 @@
+// operators
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/4)
+// console.log(3%4)
+
+let string1 = "Mohammad"
+let string2 = " Raza"
+let string3 = string1 + string2 
+console.log(string3)
+
+
+// { below these code are not appropriate, make code easy,clean and readable, not complex
+
+// console.log("1" +2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(3 + 4 * 5 % 3)
+// console.log(+true)
+// console.log(+"")
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+
+
+//prefix
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+
+//postfix
+let gameCuonter = 100
+gameCuonter++;
+console.log(gameCounter)
