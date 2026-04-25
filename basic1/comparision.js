04e01bbc36ddca66ced4b877b88bfd6db5896a70
razabhaay <razabhaay@gmail.com>
2026-04-25 11:30:01 +0000

    comparision

---
+++ basic1/comparision.js
@@ -0,0 +1,28 @@
+console.log(2 < 1)
+console.log(2 > 1)
+console.log(2 <= 1)
+console.log(2 >= 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+
+//avoid these type of comparisions below
+
+console.log("2" > 1)
+console.log(2 < "01")
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null == 0)
+console.log(null <= 0)
+console.log(null != 0)
+console.log(null >= 0)
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined == 0)
+console.log(undefined <= 0)
+console.log(undefined != 0)
+console.log(undefined >= 0)
+
+console.log("2" === 2)
