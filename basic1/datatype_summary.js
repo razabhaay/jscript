11ce6bd113812069d9ad8ad48c281a7e09e17c28
razabhaay <razabhaay@gmail.com>
2026-04-25 15:43:24 +0000

    datatype_summary

---
+++ basic1/datatype_summary.js
@@ -0,0 +1,22 @@
+
+
+
+// const bigNumer = 12272722234577531n
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "Raza"]
+let myObject = {
+    name: "Raza",
+    age: 22,
+}
+
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof myFunction)
