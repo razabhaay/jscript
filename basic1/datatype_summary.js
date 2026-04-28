45d93dfb1a67960fb595e158453802a1d0e3c2bd
razabhaay <razabhaay@gmail.com>
2026-04-28 10:31:19 +0000

    stack and heap

--- basic1/datatype_summary.js
+++ basic1/datatype_summary.js
@@ -20,3 +20,35 @@ const myFunction = function(){
 }
 
 console.log(typeof myFunction)
+
+
+
+// memory types
+// 1) stack memory use for primitive type
+// when U change in stack it will change in copy not in original
+// 2) heap memory use for non-primitive type
+// when U change in heap it will change in original
+
+// stack memory example
+
+let myName = "Raza"
+let otherName = myName
+otherName = "Mohammad Raza"
+
+console.log([myName, otherName]);
+console.table([myName, otherName]);
+
+// heap memory example
+
+let user1 = {
+    email : "razabhaay@gmail.com",
+    upiId : "razabhaay@ybl"
+}
+
+let user2 = user1
+
+user2.email = "mohammadraza99@gmail.com"
+user2.upiId = "mohammadraza99@ybl"
+
+console.log([user1.email, user2.email]);
+console.table([user1.email, user2.email])
