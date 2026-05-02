5c60bf03026b7a769d7cc7884afcca7f39a798ea
razabhaay <razabhaay@gmail.com>
2026-05-02 14:57:22 +0000

    array

--- basic1/Array.js
+++ basic1/Array.js
@@ -50,10 +50,51 @@ let ArrToString = ArrInString.join()
 
 //use slice to get a slice/peice/small-peice of data from the data and don't change/manipulate original data
 const ArrInSlice = ArrInNumber.slice(1,2)
-console.log(ArrInNumber);
-console.log(ArrInSlice);
+// console.log(ArrInNumber);
+// console.log(ArrInSlice);
 
 //use slpice to get a slice/peice/small-peice of data from the data and change/manipulate original data
 const ArrInSplice = ArrInNumber.splice(1,2)
-console.log(ArrInNumber);
-console.log(ArrInSplice);
+// console.log(ArrInNumber);
+// console.log(ArrInSplice);
+
+
+// ++++++++++++Array Function+++++++
+
+
+let top4Heros = ['Abubakr', 'Umar', 'Usman', 'Ali']
+let young2Heros = ['Hasan', 'Husain']
+
+//type1
+let allHeros = top4Heros.concat(young2Heros)
+// console.log(allHeros);
+
+//type2
+let all_Heros = [...top4Heros, ...young2Heros]
+// console.log(all_Heros);
+
+//type3
+const deepArr = [1, 2, 3, [4, 5, [6]], 7, 8, 9]
+
+// in flat U can give how many lairs should be merg like 1,2,3 or Infinity to all merg
+let mergDeepArr = deepArr.flat(2)
+mergDeepArr = deepArr.flat(Infinity)
+// console.log(mergDeepArr);
+
+//.isArray is use to question - is this array it will ans. in boolean_true/false
+// console.log(Array.isArray(['Mohammad', 'Raza']));
+// console.log(Array.isArray('Mohammad', 'Raza'));
+
+//use .form to show in array
+// console.log(Array.from('Mohammad Raza'));
+
+//advanced 
+console.log(Array.from({name: 'Mohammad Raza'})) //mostly use in backend
+
+let score0 = 99
+let score1 = 100
+let score2 = 199
+
+//use .of to convert to array 
+console.log(Array.of(score0, score2, score1));
+
