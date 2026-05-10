f5b74a999101b584d1a94d1ce8c118f132fb2b8d
razabhaay <razabhaay@gmail.com>
2026-05-10 11:24:21 +0000

    switch

---
+++ basic1/switch.js
@@ -0,0 +1,108 @@
+// //switch case statement
+
+// //number
+// let month = 11
+// switch (month) {
+//     case 1:
+//         console.log('jan')
+//         break;
+//     case 2:
+//         console.log('feb')
+//         break;
+//     case 3:
+//         console.log('march')
+//         break;
+//     case 4:
+//         console.log('april')
+//         break;
+//     case 5:
+//         console.log('may')
+//         break;
+//     case 6:
+//         console.log('jun')
+//         break;
+//     case 7:
+//         console.log('july')
+//         break;
+//     case 8:
+//         console.log('aug')
+//         break;
+//     case 9:
+//         console.log('sep')
+//         break;
+//     case 10:
+//         console.log('oct')
+//         break;
+//     case 11:
+//         console.log('nov')
+//         break;
+//     case 12:
+//         console.log('dec');
+//         break;
+//     default:
+//         console.log('month');
+//         break;
+// }
+
+
+// //string
+// let months = 'jan'
+// switch (month) {
+//     case 'jan':
+//         console.log('jan')
+//         break;
+//     case 'feb':
+//         console.log('feb')
+//         break;
+//     case 'mar':
+//         console.log('march')
+//         break;
+//     case 'april':
+//         console.log('april')
+//         break;
+//     case 'may':
+//         console.log('may')
+//         break;
+//     case 'june':
+//         console.log('jun')
+//         break;
+//     case 'july':
+//         console.log('july')
+//         break;
+//     case 'aug':
+//         console.log('aug')
+//         break;
+//     case 'sep':
+//         console.log('sep')
+//         break;
+//     case 'oct':
+//         console.log('oct')
+//         break;
+//     case 'nov':
+//         console.log('nov')
+//         break;
+//     case 'dec':
+//         console.log('dec');
+//         break;
+//     default:
+//         console.log('month');
+//         break;
+// }
+
+
+//if we don't give break it will print all cases
+let name = 'surname'
+switch (name) {
+    case 'surname': 
+        console.log('Shaikh');
+
+        case 'firstname': 
+        console.log('Mohammad Raza');
+
+        case 'lastname': 
+        console.log('Shamshad Alam');
+        break;
+
+    default: 'Shaikh Mohammad Raza Shamshad Alam'
+        break;
+}
